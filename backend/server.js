a0f5989702d9742025808455d3a9a72886abf05c
bolaji2274 <hammedbolajihammed@gmail.com>
2024-11-25 16:24:41 +0100

    updates added for env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express"
 import authRoutes from './routes/auth.routes.js'
 import dotenv from 'dotenv'
-import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config()
@@ -10,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // to parser the req.body
+app.use(express.urlencoded({extended: true})) // to parser the form data
 
 
 app.use("/api/auth", authRoutes)
